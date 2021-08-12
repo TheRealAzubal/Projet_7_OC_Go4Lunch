8daf54780f0430d3609ddb8f9f6b95e4bee05f6d
Azubal <valentinfamery087@gmail.com>
2021-08-12 13:49:38 +0200

    Add New Activity for Users SignIn or Login

---
+++ app/src/main/java/com/azubal/go4lunch/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.azubal.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
