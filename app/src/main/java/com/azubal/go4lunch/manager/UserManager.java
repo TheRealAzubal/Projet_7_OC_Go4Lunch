a1dbe739ba412bc78cdabf22bae5d86a89edef6c
Azubal <valentinfamery087@gmail.com>
2021-08-19 15:13:17 +0200

    Add UserManager and UserRepository for Firebase

---
+++ app/src/main/java/com/azubal/go4lunch/manager/UserManager.java
@@ -0,0 +1,36 @@
+package com.azubal.go4lunch.manager;
+
+import com.azubal.go4lunch.repository.UserRepository;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserManager {
+
+    private static volatile UserManager instance;
+    private final UserRepository userRepository;
+
+    private UserManager() {
+        userRepository = UserRepository.getInstance();
+    }
+
+    public static UserManager getInstance() {
+        UserManager result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(UserRepository.class) {
+            if (instance == null) {
+                instance = new UserManager();
+            }
+            return instance;
+        }
+    }
+
+    public FirebaseUser getCurrentUser(){
+        return userRepository.getCurrentUser();
+    }
+
+    public Boolean isCurrentUserLogged(){
+        return (this.getCurrentUser() != null);
+    }
+
+}
---
+++ app/src/main/java/com/azubal/go4lunch/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.azubal.go4lunch.repository;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserRepository {
+
+    private static volatile UserRepository instance;
+
+    private UserRepository() { }
+
+    public static UserRepository getInstance() {
+        UserRepository result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(UserRepository.class) {
+            if (instance == null) {
+                instance = new UserRepository();
+            }
+            return instance;
+        }
+    }
+
+    @Nullable
+    public FirebaseUser getCurrentUser(){
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+}
