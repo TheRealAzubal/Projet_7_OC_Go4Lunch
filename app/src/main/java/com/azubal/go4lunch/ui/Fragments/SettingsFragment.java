0938a6f9d5e204a8d25a290be760d26f1afb412c
Azubal <valentinfamery087@gmail.com>
2021-08-27 13:51:33 +0200

    Change title toolbar for YourLunch and Settings Fragment

--- app/src/main/java/com/azubal/go4lunch/ui/Fragments/SettingsFragment.java
+++ app/src/main/java/com/azubal/go4lunch/ui/Fragments/SettingsFragment.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.azubal.go4lunch.R;
+import com.azubal.go4lunch.ui.Activities.MainActivity;
 
 public class SettingsFragment extends Fragment {
+
+    MainActivity mainActivity;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -19,6 +22,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getMainActivity();
+        mainActivity.setToolbarTitle(getString(R.string.settings));
 
     }
 
@@ -27,5 +32,9 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public void getMainActivity() {
+        mainActivity = (MainActivity) getActivity();
     }
 }
--- app/src/main/java/com/azubal/go4lunch/ui/Fragments/YourLunchFragment.java
+++ app/src/main/java/com/azubal/go4lunch/ui/Fragments/YourLunchFragment.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.azubal.go4lunch.R;
+import com.azubal.go4lunch.ui.Activities.MainActivity;
 
 public class YourLunchFragment extends Fragment {
+
+    MainActivity mainActivity;
 
     public YourLunchFragment() {
         // Required empty public constructor
@@ -19,7 +22,8 @@ public class YourLunchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getMainActivity();
+        mainActivity.setToolbarTitle(getString(R.string.yourLunch));
     }
 
     @Override
@@ -27,5 +31,9 @@ public class YourLunchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_your_lunch, container, false);
+    }
+
+    public void getMainActivity() {
+        mainActivity = (MainActivity) getActivity();
     }
 }
